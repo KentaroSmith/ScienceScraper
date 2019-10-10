8b1c1a15f5408f322eea025df463085f1c61e4eb
Dan Smith <daniel.smith8795@gmail.com>
2019-10-09 23:27:06 -0500

    Cleaned up serverjs and added to readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ScienceScraper
+## By Dan Smith
+
+This is a webscraper aimed at tracking content from [NASA's press release page!](https://www.nasa.gov/content/science-mission-directorate-press-releases)
+
+The app lets the user scrape that day's articles, store them in a database, then add notes to articles in order to review them later. 
+
+### Node Packages used:
+* express 
+* handlebars 
+* logger 
+* mongoose 
+* axios 
+* cheerio 
+
+This app is also deployed live on heroku, and requires an addon to facilitate Mongodb.
+
+At the time of writing (10/9/2019) the live application is still underway, but is continuing to be worked on.
--- server.js
+++ server.js
@@ -14,17 +14,12 @@ app.use(express.json());
 app.use(express.static("public"));
 mongoose.connect("mongodb://localhost/scinews", { useNewUrlParser: true });
 app.get("/scrape", function (req, res) {
-    // First, we grab the body of the html with axios
+
     axios.get("https://www.nasa.gov/content/science-mission-directorate-press-releases").then(function (response) {
-        // Then, we load that into cheerio and save it to $ for a shorthand selector
+
         var $ = cheerio.load(response.data);
-
-        // Now, we grab every h2 within an article tag, and do the following:
         $(".ubernode").each(function (i, element) {
-            // Save an empty result object
             var result = {};
-
-            // Add the text and href of every link, and save them as properties of the result object
             result.title = $(this)
                 .children("h3")
                 .text();
@@ -32,24 +27,19 @@ app.get("/scrape", function (req, res) {
                 .children("a")
                 .attr("href");
 
-            // Create a new Article using the `result` object built from scraping
             db.Article.create(result)
                 .then(function (dbArticle) {
-                    // View the added result in the console
                     console.log(dbArticle);
                 })
                 .catch(function (err) {
-                    // If an error occurred, log it
                     console.log(err);
                 });
         });
 
-        // Send a message to the client
         res.send("Scrape Complete");
     });
 });
 
-// Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
     db.Article.find({})
         .then(function (article) {
@@ -58,13 +48,9 @@ app.get("/articles", function (req, res) {
         .catch(function (err) {
             res.json(err)
         })
-    // TODO: Finish the route so it grabs all of the articles
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function (req, res) {
-    // TODO
-    // ====
     db.Article.find({ _id: req.params.id }).populate("note")
         .then(function (id) {
             res.json(id)
@@ -72,18 +58,9 @@ app.get("/articles/:id", function (req, res) {
         .catch(function (err) {
             res.json(err)
         })
-    // Finish the route so it finds one article using the req.params.id,
-    // and run the populate method with "note",
-    // then responds with the article with the note included
 });
 
-// Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
-    // TODO
-    // ====
-    // save the new note that gets posted to the Notes collection
-    // then find an article from the req.params.id
-    // and update it's "note" property with the _id of the new note
     db.Note.create(req.body)
         .then(function (dbnote) {
             return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbnote._id }, { new: true })
@@ -97,7 +74,6 @@ app.post("/articles/:id", function (req, res) {
 })
     ;
 
-// Start the server
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
 });
